473542e7567bb6eacdc1063aa0bb19ad710e1fc3
mtSAheSK <kotashorts2010@gmail.com>
2025-07-05 16:56:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",// session id 💙
-HACKER : process.env.HACKER || "94704638406" //Addmin number 💙
+SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=tilHgbDC#ToejGB9Cqza-CdtQ_a-OcMstyuKwwB3r-MLJNij7_YU",// session id 💙
+HACKER : process.env.HACKER || "94787460931" //Addmin number 💙
 };
